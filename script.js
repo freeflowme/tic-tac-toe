034cfc3288e4d7bc8953f2cbaec1d66b9eb07c7a
freeflowme <116676605+freeflowme@users.noreply.github.com>
2023-03-20 18:30:51 -0500

    Add initial resetBtn functionality

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const GameBoard = (() => {
     const board = [];
+
+    const playX = document.querySelectorAll('.gameSquare');
+        playX.forEach(gameSquare => {
+            gameSquare.addEventListener('click', () => {
+                const x = document.createElement('div');
+                x.className = 'X';
+                x.textContent = 'X';
+                board.push(x.textContent);
+                console.log(board);
+                gameSquare.appendChild(x);
+            })
+        })
+
+    const reset = document.querySelector('#resetBtn');
+        resetBtn.addEventListener('click', () => {
+            const clearBoard = document.querySelectorAll('.gameSquare');
+                clearBoard.forEach(gameSquare => {
+                    while (gameSquare.firstChild) {
+                        gameSquare.firstChild.remove();
+                    }
+                })
+                while (board.length > 0) {
+                    board.shift();
+                }
+                console.log(board);
+        })
+
 })();
 
 const Player = (name) => {
     return name;
 };
+
